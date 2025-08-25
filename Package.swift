3c0f3260d31d80a6adbe80a15d235ba624372cdf
MaikMars <miquelmarset91@gmail.com>
2025-08-25 14:38:04 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,11 +7,11 @@ let package = Package(
      .iOS(.v14),
    ],
    products: [
-      .library(name: "PaintCodexMultiplatform", targets: ["PaintCodexMultiplatform"])
+      .library(name: "paintCodexMultiplatform", targets: ["PaintCodexMultiplatform"])
    ],
    targets: [
       .binaryTarget(
-         name: "PaintCodexMultiplatform",
+         name: "paintCodexMultiplatform",
          url: "https://github.com/MaikMars/PaintCodexMultiplatform/releases/download/1.0.5/paintCodexMultiplatform.xcframework.zip",
          checksum:"8feecbd09be3416574a991f2b1a005d748345bcefb154cfab6c6521e8c5f10c9")
    ]
